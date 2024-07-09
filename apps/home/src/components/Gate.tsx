9d7e438472c959a7bff9bc39b943a4308680b597
rhesus2016 <rhesus2016@gmail.com>
2024-07-09 23:46:00 +0900

    feat: Gate Component 추가

---
+++ apps/home/src/components/Gate.tsx
@@ -0,0 +1,38 @@
+import { Text } from '@29cm/ui-emotion';
+import { vars } from '@29cm/ui-tokens';
+import styled from '@emotion/styled';
+import { default as NextImage } from 'next/image';
+
+type GateProps = {
+  image: string;
+  label: string;
+  icon: JSX.Element;
+};
+
+const Gate = ({ image, label, icon }: GateProps) => {
+  return (
+    <Container>
+      <Image width={38} height={38} src={image} alt={label} />
+      <Text color="primary" typography="text-l-medium">
+        {label}
+      </Text>
+      {icon}
+    </Container>
+  );
+};
+
+export default Gate;
+
+const Container = styled.div`
+  display: flex;
+  gap: 8px;
+  width: fit-content;
+  align-items: center;
+  border: 1px solid ${vars.$semantic.color.border.line};
+  border-radius: 9999px;
+  padding: 4px 16px 4px 4px;
+`;
+
+const Image = styled(NextImage)`
+  border-radius: 9999px;
+`;
